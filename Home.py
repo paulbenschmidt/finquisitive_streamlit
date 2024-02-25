4de09878160b25cf953ea4f864e192a8bebc5fc6
Paul Schmidt <paul.schmidt@homechef.com>
2024-02-24 20:37:19 -0500

    finish home page

--- Home.py
+++ Home.py
@@ -12,7 +12,14 @@ st.write("# Welcome to FinQuisitive! 👋")
 # TODO: Create authentication (to capture email)
 # TODO: Create donation page
 
-# TODO: WRite about how this tool is a combo of book + chart, meant to teach financial principles in an interactive way
 st.markdown("""
-    FinQuisitive is a financial literacy application designed to interactively teach financial principles.
+    FinQuisitive is a financial literacy application designed to teach financial principles interactively.
+
+    While there are many books and courses that teach financial literacy, we felt that there was a lack of
+    interactive tools that could help people learn about finance in a more engaging and relatable way. We hope that
+    FinQuisitive can fill that gap. And we hope that having a framework to think about your future will inspire
+    you to make the most of your days in order to live a life of abundance, for yourself and those around you.
+
+    With love and gratitude,
+    The FinQuisitive Team
 """)
