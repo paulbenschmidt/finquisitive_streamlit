470509107929752fe5c63278ecfb823aacf3828a
Paul Schmidt <paul.schmidt@homechef.com>
2024-03-13 11:42:53 -0400

    remove settings menu

--- helper/page_setup.py
+++ helper/page_setup.py
@@ -9,3 +9,12 @@ def setup_page():
         page_icon=Image.open("./static/logo_black.png"),
         layout="wide"
     )
+
+    hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+
+    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
